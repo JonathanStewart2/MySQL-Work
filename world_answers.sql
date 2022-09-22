95fdc0b0b1460bb0a1b1a751ce5d15bb082557c4
JonathanStewart2 <jonathan.stewart2@hotmail.com>
2022-09-22 15:27:42 +0100

    updating answers

--- world_answers.sql
+++ world_answers.sql
@@ -8,13 +8,16 @@ SELECT * FROM city;
 SELECT DISTINCT CountryCode FROM city;
 SELECT COUNT(id) FROM city WHERE CountryCode = "USA";  #274
 
+
 ### QUESTION 2- 2.	Find out the population and life expectancy for people in Argentina.
 SELECT Population, LifeExpectancy FROM country WHERE Code = "ARG";   #37032000   75.1
+
 
 ### QUESTION 3- 3.	Using IS NOT NULL, ORDER BY, and LIMIT,
 # which country has the highest life expectancy?
 SELECT * FROM country;
 SELECT Name, LifeExpectancy FROM country ORDER BY LifeExpectancy DESC LIMIT 1;
+
 
 ### QUESTION 4.	Using JOIN ... ON, find the capital city of Spain.
 SELECT * FROM country;
@@ -28,11 +31,20 @@ JOIN country ON c.ID=country.Capital
 WHERE ID=(
 	SELECT Capital FROM country WHERE Name="Spain");
 
-### QUESTION 5: 5.	Using JOIN ... ON, list all the languages spoken in the Southeast Asia region.
-SELECT * FROM countrylanguage;
-SELECT * FROM country;
 
+### QUESTION 5: 5.	Using JOIN ... ON, list all the languages spoken in the Southeast Asia region.
 SELECT c.Code FROM country c WHERE Region="Southeast Asia";
 
 SELECT DISTINCT cl.Language FROM countrylanguage cl
 JOIN country c ON c.Code = cl.CountryCode
+WHERE c.Region="Southeast Asia";
+
+
+### QUESTION 6: Using a single query, list 25 cities around the world that start with the letter F.
+SELECT c.Name FROM city c WHERE c.Name LIKE "F%" LIMIT 25;
+
+
+### QUESTION 7: Using COUNT and JOIN ... ON, get the number of cities in China.
+SELECT * FROM countrylanguage;
+SELECT * FROM city;
+SELECT * FROM country;
