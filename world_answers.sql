e2a0abbe4dbcc80c8f48ecc5f56c69cacd2c90fc
JonathanStewart2 <jonathan.stewart2@hotmail.com>
2022-09-22 15:13:54 +0100

    adding answers

---
+++ world_answers.sql
@@ -0,0 +1,38 @@
+USE world;
+
+SHOW tables;
+DESCRIBE city;
+
+### QUESTION 1- 1.	Using COUNT, get the number of cities in the USA.
+SELECT * FROM city;
+SELECT DISTINCT CountryCode FROM city;
+SELECT COUNT(id) FROM city WHERE CountryCode = "USA";  #274
+
+### QUESTION 2- 2.	Find out the population and life expectancy for people in Argentina.
+SELECT Population, LifeExpectancy FROM country WHERE Code = "ARG";   #37032000   75.1
+
+### QUESTION 3- 3.	Using IS NOT NULL, ORDER BY, and LIMIT,
+# which country has the highest life expectancy?
+SELECT * FROM country;
+SELECT Name, LifeExpectancy FROM country ORDER BY LifeExpectancy DESC LIMIT 1;
+
+### QUESTION 4.	Using JOIN ... ON, find the capital city of Spain.
+SELECT * FROM country;
+SELECT * FROM city;
+
+SELECT Name FROM city  WHERE ID=(
+	SELECT Capital FROM country WHERE Name="Spain");
+    
+SELECT c.Name FROM city c
+JOIN country ON c.ID=country.Capital
+WHERE ID=(
+	SELECT Capital FROM country WHERE Name="Spain");
+
+### QUESTION 5: 5.	Using JOIN ... ON, list all the languages spoken in the Southeast Asia region.
+SELECT * FROM countrylanguage;
+SELECT * FROM country;
+
+SELECT c.Code FROM country c WHERE Region="Southeast Asia";
+
+SELECT DISTINCT cl.Language FROM countrylanguage cl
+JOIN country c ON c.Code = cl.CountryCode
